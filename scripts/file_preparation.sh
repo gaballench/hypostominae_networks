d37507d0d532c8dab9610b00fedba5a89914d360
gaballench <gaballench@gmail.com>
2021-12-23 19:59:31 +0000

    Faked an outgroup for testing purposes. Corrected several bugs

--- scripts/file_preparation.sh
+++ scripts/file_preparation.sh
@@ -2,10 +2,10 @@
 
 # create a simple log file for recording which files are being created and where
 touch fileops.log
-cat "nexus\tmrbayes\tdir" > fileops.log
+echo "nexus\tmrbayes\tdir" > fileops.log
 
 # create the directory for each job
-mkdir job1 job2 job3 job4
+mkdir job1 job2 job3
 
 # set the counter var
 COUNTER=1
@@ -13,18 +13,19 @@ COUNTER=1
 # process each nexus file to create the script and move them to the appropriate dir
 for i in `ls *.nex`; do
     # reset the counter to 1 if it's higher than 4
-    if [[ "$COUNTER" -gt 4 ]]; then
+    if [[ "$COUNTER" -gt 3 ]]; then
 	let "COUNTER=1"
     fi
     # create the mb script from the template file and just append the extension .mb
-    cp template.mb $i.mb
+    touch $i.mb
+    cat ../scripts/template.mb > $i.mb
     # replace the placeholder ALIGNMENT with the alignment filename
-    sed -i 's/ALIGNMENT/$i/g' $i.mb
+    sed -i "s/ALIGNMENT/$i/g" $i.mb
     # move the script and alignment to the job dir
     mv $i "job$COUNTER"
     mv $i.mb "job$COUNTER"
     # record filenames to the logger
-    cat "$i\t$i.mb\tjob$COUNTER" >> fileops.log
+    echo "$i\t$i.mb\tjob$COUNTER" >> fileops.log
     # add one to the counter for entering the next folder
     let "COUNTER+=1"
 done
--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 # move to the data dir
-cd data
+cd ../data
 # run the file preparator script from the data dir
 ../scripts/./file_preparation.sh
