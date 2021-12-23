9f34d98bcb08e4d16f2a968efddb1fad8588d2fd
gaballench <gaballench@gmail.com>
2021-12-23 19:47:08 +0000

    Setting the scripts and ready to test on fake nexus files

---
+++ scripts/file_preparation.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+# create a simple log file for recording which files are being created and where
+touch fileops.log
+cat "nexus\tmrbayes\tdir" > fileops.log
+
+# create the directory for each job
+mkdir job1 job2 job3 job4
+
+# set the counter var
+COUNTER=1
+
+# process each nexus file to create the script and move them to the appropriate dir
+for i in `ls *.nex`; do
+    # reset the counter to 1 if it's higher than 4
+    if [[ "$COUNTER" -gt 4 ]]; then
+	let "COUNTER=1"
+    fi
+    # create the mb script from the template file and just append the extension .mb
+    cp template.mb $i.mb
+    # replace the placeholder ALIGNMENT with the alignment filename
+    sed -i 's/ALIGNMENT/$i/g' $i.mb
+    # move the script and alignment to the job dir
+    mv $i "job$COUNTER"
+    mv $i.mb "job$COUNTER"
+    # record filenames to the logger
+    cat "$i\t$i.mb\tjob$COUNTER" >> fileops.log
+    # add one to the counter for entering the next folder
+    let "COUNTER+=1"
+done
+
---
+++ scripts/pipeline.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# move to the data dir
+cd data
+# run the file preparator script from the data dir
+../scripts/./file_preparation.sh
