52e496201d0f2c0060bb52042788f49b9589a367
gaballench <gaballench@gmail.com>
2021-12-23 20:59:53 +0000

    Example ready with some fake data with the same number of taxa and comparable number of characters. It should generate the scripts and submit automatically. Testing in the cluster now

--- scripts/file_preparation.sh
+++ scripts/file_preparation.sh
@@ -2,10 +2,10 @@
 
 # create a simple log file for recording which files are being created and where
 touch fileops.log
-echo "nexus\tmrbayes\tdir" > fileops.log
+echo "nexus mrbayes dir" > fileops.log
 
 # create the directory for each job
-mkdir job1 job2 job3
+mkdir job1 job2 job3 job4
 
 # set the counter var
 COUNTER=1
@@ -13,7 +13,7 @@ COUNTER=1
 # process each nexus file to create the script and move them to the appropriate dir
 for i in `ls *.nex`; do
     # reset the counter to 1 if it's higher than 4
-    if [[ "$COUNTER" -gt 3 ]]; then
+    if [[ "$COUNTER" -gt 4 ]]; then
 	let "COUNTER=1"
     fi
     # create the mb script from the template file and just append the extension .mb
@@ -25,8 +25,17 @@ for i in `ls *.nex`; do
     mv $i "job$COUNTER"
     mv $i.mb "job$COUNTER"
     # record filenames to the logger
-    echo "$i\t$i.mb\tjob$COUNTER" >> fileops.log
+    echo "$i $i.mb job$COUNTER" >> fileops.log
     # add one to the counter for entering the next folder
     let "COUNTER+=1"
 done
 
+# prepare the pbs scripts using the template file template.pbs
+for i in `ls -d job*`; do
+    # create the pbs script from the template file and just append the extension .pbs
+    touch $i.pbs
+    cat ../scripts/template.pbs > $i.pbs
+    # replace the placeholder ALIGNMENT with the alignment filename
+    sed -i "s/JOBDIR/$i/g" $i.pbs
+    mv $i.pbs $i
+done
--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -4,3 +4,12 @@
 cd ../data
 # run the file preparator script from the data dir
 ../scripts/./file_preparation.sh
+
+# visit each job dir and submit the job
+for i in `ls -d job*`; do
+    cd $i
+    echo $i
+    ls *.pbs
+    qsub *.pbs
+    cd ..
+done
