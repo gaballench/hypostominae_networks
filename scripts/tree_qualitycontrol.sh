2c2c94048af6c4fee8a97901dd311ac87df3d62a
gaballench <gaballench@gmail.com>
2022-01-16 22:20:09 +0000

    quality control script now redirects to a general file in order to avoid loading individual-job files in R

--- scripts/tree_qualitycontrol.sh
+++ scripts/tree_qualitycontrol.sh
@@ -6,7 +6,7 @@ for j in `ls -d job*`; do
     echo "Processing $j..."
     for i in `ls *.log`; do
 	sdsf=$(grep -a "    Average standard deviation of split frequencies = " $i | awk '{split($0, array, " "); print array[8]}')
-	echo "$i $sdsf" >> topostability.txt
+	echo "$j $i $sdsf" >> ../topostability.txt
     done
     cd ../
 done
